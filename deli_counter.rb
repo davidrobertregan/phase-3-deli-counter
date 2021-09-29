8aa8a88225d55bbcec5bb5827ae48fb9dc526679
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-09-29 15:08:56 -0500

    commit tree ab4847cd1fea8fd73f45b6fd02b44fa16cda1666

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+# Write your code here
+require 'pry'
+
+def line array
+    if array != []
+        line = array.each_with_index.map {|e, i| "#{i + 1}. #{e}" }.join(" ")
+        puts "The line is currently: #{line}"
+        
+    else
+        puts "The line is currently empty."
+    end
+end 
+
+def take_a_number array, name
+    array.push(name)
+    puts "Welcome, #{name}. You are number #{array.length} in line."
+end
+
+def now_serving array
+    if array.first
+        puts "Currently serving #{array.first}."
+        array.shift
+    else 
+        puts "There is nobody waiting to be served!"
+    end
+end
+
+array =  ['bill', 'john', 'katie']
+
+take_a_number array, "dave"
